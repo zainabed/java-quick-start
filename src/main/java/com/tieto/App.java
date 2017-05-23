fb40c66f64ffdea00394180ab571143b5f49e3df
Zainabed <zainabed@gmail.com>
2017-05-23 12:38:30 +0530

    Initial commit

---
+++ src/main/java/com/tieto/App.java
@@ -0,0 +1,13 @@
+package com.tieto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

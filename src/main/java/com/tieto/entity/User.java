f5dd2dfdee1f37fdcdb9941a55ef986040dfbbad
zainabed <zainabed@gmail.com>
2017-06-04 19:34:00 +0530

    Removed student class artifacts.

--- src/main/java/com/tieto/entity/User.java
+++ src/main/java/com/tieto/entity/User.java
@@ -1,22 +1,7 @@
 package com.tieto.entity;
 
 public class User {
-	 private String id;
-	 private String username;
-	 
-   protected void setId(String id){
-  	 this.id = id;
-   }
-   
-   public String getId(){
-  	 return this.id;
-   }
-   
-   public void setUsername(String username) {
-  	 this.username = username;
-   }
-   
-   public String getUsername(){
-  	 return this.username;
-   }
+	 public String getId(){
+		 return "";
+	 }
 }
